47ddb81bb4716679b109c78744a6a3b8648c0def
Prasoon Kumar <prasoonkumar467@gmail.com>
2024-07-17 13:35:12 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+let name = "prasoon"   // string => " "
+let age = 19           // number => upto 2^53 digits
+                       // bigint => bigger values than number 
+
+let isLoggedIn = true  // boolean => true/false
+let state = null       // null => standalone value / empty / no value
+let gender;            // undefined => value not asinged
+
+// symbol => unique
+// object
+
+console.log(typeof age)  // print the datatype of the given
+
+console.log(typeof null);   // object
+console.log(typeof undefined);  // undefined
