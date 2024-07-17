005ac40bf1693cb681d5ba0428ff55ad33d69c9d
Prasoon Kumar <prasoonkumar467@gmail.com>
2024-07-17 13:53:34 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,24 @@
+let score = "22"
+
+console.log(typeof score)
+
+let valueInNum = Number(score) // converting score variable into a Number datatype
+
+console.log(typeof valueInNum)
+
+/* conversions
+
+"33" =>
+"33abc" => NaN (Not a Number)
+null => 0
+undefined => NaN
+true => 1; false => 0
+
+*/ 
+
+/*
+Conversion to Boolean
+1 => true; 0 => false
+"" => false
+"text" => true 
+*/
