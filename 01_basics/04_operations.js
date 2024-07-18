1260a9318a8fd7366d0395be5cab4abcfe1f147f
Prasoon Kumar <prasoonkumar467@gmail.com>
2024-07-18 13:52:35 +0530

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,41 @@
+let value = 33
+
+let negvalue= -value
+console.log(negvalue);   // -33
+
+
+console.log(2+2);   // add
+console.log(2-2);   // subtract
+console.log(2*2);   // multiply
+console.log(2**3);  // power
+console.log(2/2);   // divide(quotient)
+console.log(2%2);   // remainder after devide
+
+
+let str1 = "hello"
+let str2 = " buddy!"
+
+let str3 = str1 + str2
+console.log(str3);     // hello buddy!
+
+
+console.log("1" + 2);   // 12
+console.log(1 + "2");   // 12
+console.log("1" + 2 + 2);   // 122  // if string is first then everything will be treated as a string
+console.log(1 + 2 + "2");   // 32  // but if string is at last then all conversion/operations will be done normally
+
+
+console.log(+ true);
+console.log(+ "");
+
+
+let num1, num2, num3 
+
+
+let gameCounter = 100
+++gameCounter;  // prefix operator
+gameCounter++;  // postfix operator
+console.log(gameCounter);
+
+// study about prefix and postfix operator from mdn
+// always write clean and readable code
