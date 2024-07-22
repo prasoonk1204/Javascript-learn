a73c37adcf538c53a09b1b90893f655f2a4b2eca
Prasoon Kumar <prasoonkumar467@gmail.com>
2024-07-22 08:04:40 +0000

    functions intro

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -9,7 +9,7 @@ function sayMyName() {
     
 }   // defining a function
 
-// sayMyName()   // executing function
+sayMyName()   // executing function
 
 // function addTwoNumbers (number1, number2) {
 //     console.log(number1 + number2);
@@ -26,7 +26,7 @@ function addTwoNumbers (number1, number2) {
 
 const result = addTwoNumbers(3, 5)
 
-// console.log(`Result = ${result}`);
+console.log(`Result = ${result}`);
 
 // number1, number2 are called parameters -- values while defining function
 // 3, 5 are called arguments -- values while calling the function
