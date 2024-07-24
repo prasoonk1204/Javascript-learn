3ed8ee9bf98b40dbfdd6449cd75a57cc072187dd
Prasoon Kumar <prasoonkumar467@gmail.com>
2024-07-24 10:59:51 +0000

    switch completed

---
+++ 04_controlFlow.js/switch.js
@@ -0,0 +1,52 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }                        // Syntax
+
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log(`January`);
+        break;
+    case 2:
+        console.log(`February`);
+        break;
+    case 3:
+        console.log(`March`);
+        break;
+    case 4:
+        console.log(`April`);
+        break;
+
+    default:
+        console.log(`No match`);
+        break;
+}
+
+
+const monthInAlpha = "mar"
+
+switch (monthInAlpha) {
+    case "jan":
+        console.log(`January`);
+        break;
+    case "feb":
+        console.log(`February`);
+        break;
+    case "mar":
+        console.log(`March`);
+        break;
+    case "apr":
+        console.log(`April`);
+        break;
+
+    default:
+        console.log(`No match`);
+        break;
+}
