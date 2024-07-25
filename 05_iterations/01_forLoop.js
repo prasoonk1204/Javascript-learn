c91b391438b728b0d6692cbbd4c954689ac4515d
Prasoon Kumar <prasoonkumar467@gmail.com>
2024-07-25 14:02:54 +0000

    for loop + break, continue

--- 05_iterations/01_forLoop.js
+++ 05_iterations/01_forLoop.js
@@ -1,4 +1,4 @@
-// for loop syntax
+// "for" loop syntax
 
 // for (let index = 0; index < array.length; index++) {
 //     const element = array[index];
@@ -8,7 +8,52 @@
 for (let i = 0; i <= 10; i++) {
     const element = i;
     if ( element == 5 ) {
-        console.log("5 is best number");
+        // console.log("5 is best number");
     }
-    console.log(element);
+    // console.log(element);
+}
+
+for (let i = 0; i <= 10; i++) {
+    // console.log(`Outer loop value ${i}`);
+    for (let j = 0; j <= 10; j++) {
+        // console.log(`Inner loop value ${j}`);
+        
+    }  
+}
+
+
+// Printing Tables from 0 to 10
+for (let i = 0; i <= 10; i++) {
+    // console.log(`Table of ${i}`);
+    for (let j = 0; j <= 10; j++) {
+        // console.log(`${i} * ${j} = ${i*j}`);
+    }   
+}
+
+
+let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+}
+
+
+// break and continue
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        // console.log(`Detected 5`);
+        break
+    }
+    // console.log(`Value of i is ${index}`);
+}
+
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue
+    }
+    console.log(`Value of i is ${index}`);
 }
