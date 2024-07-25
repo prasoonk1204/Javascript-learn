6fa838bee51b66303a2080dd47d1ee55978c6e4e
Prasoon Kumar <prasoonkumar467@gmail.com>
2024-07-25 14:15:06 +0000

    while and dowhile loops

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,29 @@
+// "while" loop syntax
+
+// let index = 0
+// while (index <= 10) {
+
+//     index++
+// }
+
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of index is ${index}`);
+    index = index+2
+}
+
+let myArray = ["flash", "batman", "superman"]
+let i=0
+while (i < myArray.length) {
+    // console.log(`Value is ${myArray[i]}`);
+    i++
+}
+
+
+// "do while" loop --> first work is done then condition is checked
+
+let score = 11
+do {
+    // console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
