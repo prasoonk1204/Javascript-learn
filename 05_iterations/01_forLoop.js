6be0933410e17fbef41d7b8532dd6df22a2de154
Prasoon Kumar <prasoonkumar467@gmail.com>
2024-07-25 08:46:14 +0000

    for loops basics

---
+++ 05_iterations/01_forLoop.js
@@ -0,0 +1,14 @@
+// for loop syntax
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if ( element == 5 ) {
+        console.log("5 is best number");
+    }
+    console.log(element);
+}
