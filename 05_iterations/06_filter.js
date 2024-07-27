43e7f76bca5960d22660f7ad1b5bd97c3487e0f5
Prasoon Kumar <prasoonkumar467@gmail.com>
2024-07-27 08:53:55 +0000

    filter

---
+++ 05_iterations/06_filter.js
@@ -0,0 +1,13 @@
+// const coding = ["js", "ruby", "python", "java", "cpp"]
+
+// const values= coding.forEach( (item) => {
+//     console.log(item);
+//     // return item // forEach does not return any value
+// } )
+
+// console.log(values); 
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myNums.filter( (num) => num > 4 )
+console.log(newNums);
